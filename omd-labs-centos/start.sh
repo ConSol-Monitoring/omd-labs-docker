9b45e25e2f4c7444f389bb1a99770545e6d3203b
Simon Meggle <simon.meggle@consol.de>
2016-12-13 12:17:29 +0100

    Full rewrite

---
+++ omd-labs-centos/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if dropin role is defined
+if [ `ls -1 /root/ansible_omd/dropin_role/ 2>/dev/null | wc -l` -gt 0 ]; then
+  echo "omd-labs: Executing Ansible drop-in role..."
+  /omd/sites/demo/bin/ansible-playbook -i localhost, /root/ansible_omd/playbook.yml -c local -e ANSIBLE_DROPIN_ROLE=$ANSIBLE_DROPIN_ROLE $ANSIBLE_VERBOSITY
+else
+  echo "No Ansible drop-in role defined, nothing to do."
+fi
+
+echo "omd-labs: Starting site..."
+echo "----------------------"
+omd start demo
+echo "----------------------"
+
+
+echo "omd-labs: Starting Apache web server..."
+exec /usr/sbin/httpd -D FOREGROUND
--- omd-labs-debian/ansible/playbook_starter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "omd-labs: Executing ansible-playbook..."
-echo "========================================"
-/omd/sites/demo/bin/ansible-playbook -i localhost, /root/ansible/playbook.yml -c local -e ANSIBLE_DROPIN_ROLE=$ANSIBLE_DROPIN_ROLE $ANSIBLE_VERBOSITY
-echo "omd-labs: Starting Apache web server..."
-echo "========================================"
-exec /usr/sbin/apache2ctl -D FOREGROUND
