6d7584dddbf1579a61ad983158c04da649f3eb8a
Simon Meggle <simon.meggle@consol.de>
2017-08-02 12:34:38 +0200

    Sync OMD data with lsyncd (centos)

--- scripts/centos/start.sh
+++ scripts/centos/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+export OMD_ROOT=/opt/omd/sites/$SITENAME
 source /root/.sitename.env
 
 echo "Config and start OMD site: $SITENAME"
@@ -7,26 +7,53 @@ echo "--------------------------------------"
 
 trap "omd stop $SITENAME; exit 0" SIGKILL SIGTERM SIGHUP SIGINT EXIT
 
-echo "Data volume check..."
+
+# mounts leer => synce dirs in mounts       / lsyncd dir->mount
+# mounts gefüllt => synce mounts in dirs    / lsyncd dir->mount
+# keine mounts => mache nix                 / kein lsyncd
+
+echo "Checking for volume mounts..."
 echo "--------------------------------------"
 for dir in "local" "etc" "var"; do
-  datadir="/opt/omd/sites/$SITENAME/$dir"
-  if mount | grep -q "$datadir"; then
-    # folder is mounted from volume
-    echo " * [EXTERNAL] $datadir"
-    if [ ! "$(ls -A $datadir)" ]; then
-        # Populate etc/ directory
-        echo " * [EXTERNAL] $datadir is empty -> initialize it"
-        echo "--------------------------------------"
-        rsync -a "$datadir.ORIG/" "$datadir/"
-    fi
-    chown -R "$SITENAME:$SITENAME" "$datadir"
+  d_local="$OMD_ROOT/$dir"
+  d_mount="$OMD_ROOT/${dir}.mount"
+  if [ ! -d "$d_mount" ]; then
+    # no volume mount
+    echo " * $dir/: [No Volume]"
   else
-    # no volume mounts, move ORIG folder back to its default location
-    mv $datadir{.ORIG,}
-    echo " * [LOCAL]    $datadir"
+    # volume mount exists
+    echo " * $dir/: [Volume] at $d_mount"
+    if [ ! "$(ls -A $d_mount)" ]; then
+        # mount is empty => sync dir in mount
+        echo "   => $dir.mount is empty; initial sync from local $dir ..."
+        rsync -rlptD --quiet "$d_local/" "$d_mount"
+        [ $? -gt 0 ] && echo "ERROR!"
+    else
+        # mount contains data => sync mount in dir
+        echo "   <= Volume contains data; sync into local $dir ..."
+        rsync -rlptD --quiet "$d_mount/" "$d_local"
+        [ $? -gt 0 ] && echo "ERROR!"
+        chown -R $SITENAME:$SITENAME "$d_local"
+    fi
+    echo "      writing the lsyncd config for $dir.mount..."
+    cat >>$OMD_ROOT/.lsyncd <<EOF
+sync {
+   default.rsync,
+   source = "${d_local}/",
+   target = "${d_mount}",
+   delay  = 0
+}
+EOF
   fi
 done
+
+echo
+
+if [ -f $OMD_ROOT/.lsyncd ]; then
+  echo "lsyncd: Starting lsyncd ..."
+  echo "--------------------------------------"
+  su - $SITENAME -c 'lsyncd ~/.lsyncd'
+fi
 
 echo
 
